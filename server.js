2cf3ee26338f9ab8d707b777659412271579a86d
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-02-15 07:44:19 -0700

    npm initilaized with dependencies. Basic server created

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-var express = require("express")
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var app = express();
+var logger = require("morgan")
+
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended:false
+    })
+);
+
+var exphbs=require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("listening on port " + port);
+});
