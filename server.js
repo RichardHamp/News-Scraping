5565b75279c806ee1382ddabb1a5a6f9213962e3
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-02-15 07:52:35 -0700

    article model complete

--- server.js
+++ server.js
@@ -11,9 +11,19 @@ app.use(
     })
 );
 
+app.use(express.static(process.cwd() + "/public"));
+
 var exphbs=require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/scraped_news");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function(){
+    console.log("Connected to Mongoose!");
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
